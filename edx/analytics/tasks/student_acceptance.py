66eb9694412a759ead9ed7719d59c785a96ba297
Rene Sorel <rene@proversity.org>
2016-07-07 10:10:24 +0200

    insert to mysql with unique views

--- edx/analytics/tasks/student_acceptance.py
+++ edx/analytics/tasks/student_acceptance.py
@@ -48,9 +48,9 @@ class StudentAcceptanceDataTask(EventLogSelectionMixin, MapReduceJobTask):
         else:
             return
 
-        breadcrumbs = path.strip('/').split('/')
+        breadcrumbs = path.strip('/').strip().split('/')
 
-        yield ((course_id, breadcrumbs[3], breadcrumbs[4], (int(breadcrumbs[5] or 0) if len(breadcrumbs) == 6 else 0), username), (date_string))
+        yield ((course_id, breadcrumbs[3], breadcrumbs[4], ((breadcrumbs[5] or 0) if len(breadcrumbs) == 6 else 0), username), (date_string))
 
     def reducer(self, key, events):
         """Calculate counts for events corresponding to course and (sub)section in a given time period."""
@@ -63,7 +63,7 @@ class StudentAcceptanceDataTask(EventLogSelectionMixin, MapReduceJobTask):
             course_id.encode('utf-8'),
             section.encode('utf8'),
             subsection.encode('utf8'),
-            unit,
+            unit.encode('utf8'),
             username.encode('utf8'),
             num_views
         )
@@ -72,7 +72,7 @@ class StudentAcceptanceDataTask(EventLogSelectionMixin, MapReduceJobTask):
         return get_target_from_url(self.output_root)
 
 
-class StudentAcceptanceTask(EventLogSelectionDownstreamMixin, MapReduceJobTaskMixin, HiveTableTask):
+class StudentAcceptanceTableTask(EventLogSelectionDownstreamMixin, MapReduceJobTaskMixin, HiveTableTask):
     """Hive table that stores the count of subsection views in each course over time."""
 
     @property
@@ -85,7 +85,7 @@ class StudentAcceptanceTask(EventLogSelectionDownstreamMixin, MapReduceJobTaskMi
             ('course_id', 'STRING'),
             ('section', 'STRING'),
             ('subsection', 'STRING'),
-            ('unit', 'INT'),
+            ('unit', 'STRING'),
             ('username', 'STRING'),
             ('num_views', 'INT')
         ]
@@ -103,3 +103,62 @@ class StudentAcceptanceTask(EventLogSelectionDownstreamMixin, MapReduceJobTaskMi
             pattern=self.pattern,
             output_root=self.partition_location
         )
+
+
+class StudentAcceptanceTask(HiveQueryToMysqlTask):
+    """Hive table that stores the count of subsection views in each course over time."""
+
+    @property
+    def table(self):
+        return 'student_acceptance'
+
+    columns = [
+        ('course_id', 'STRING'),
+        ('section', 'STRING'),
+        ('subsection', 'STRING'),
+        ('unit', 'STRING'),
+        ('num_unique_views', 'INT'),
+        ('num_views', 'INT')
+    ]
+
+    @property
+    def auto_primary_key(self):
+        # Instead of using an auto incrementing primary key, we define a custom compound primary key. See keys() defined
+        # below. This vastly improves the performance of our most common query pattern.
+        return None
+
+    @property
+    def keys(self):
+        """
+        Combine mulitple fields that must be unique together as the primary key for this table.
+        This dramatically speeds up access times at the cost of write speed.
+        """
+        # max length for this key must be under 3072 bytes; see comment in module_engagement.py
+        return [
+            ('PRIMARY KEY', ['course_id', 'section', 'subsection', 'unit'])
+        ]
+
+    @property
+    def required_table_tasks(self):
+        return StudentAcceptanceTableTask(
+            mapreduce_engine=self.mapreduce_engine,
+            n_reduce_tasks=self.n_reduce_tasks,
+            source=self.source,
+            interval=self.interval,
+            pattern=self.pattern
+        )
+
+    @property
+    def query(self):
+        return """
+            SELECT course_id, section, subsection, unit, COUNT(*) AS num_unique_views, SUM(num_views)
+            FROM student_acceptance
+            WHERE end_date >= '{start_date}' AND end_date < '{end_date}'
+        """.format(
+            start_date=self.interval.date_a.isoformat(),
+            end_date=self.interval.date_b.isoformat(),
+        )
+
+    @property
+    def partition(self):
+        return HivePartition('dt', self.interval.date_b.isoformat())  # pylint: disable=no-member
